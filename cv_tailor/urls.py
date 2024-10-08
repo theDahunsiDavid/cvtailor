4b82c4fd1f523059121665f35d7e37e8aeb4a30c
dahunsi-dami <gbemigadare@gmail.com>
2024-10-08 02:46:43 +0100

    Remove all django files to start a new project.

--- cv_tailor/urls.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Module to route data upload to upload view for processing."""
-from django.urls import path
-from .views import index, upload_view
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('upload/', upload_view, name='upload_view'),
-]

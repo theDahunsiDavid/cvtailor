5c245726fbb415777fb5b5c2291eec8bc59abeb9
dahunsi-dami <gbemigadare@gmail.com>
2024-10-08 01:12:27 +0100

    Add URL pattern for home_view to home page.

--- cv_tailor/urls.py
+++ cv_tailor/urls.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Module to route data upload to upload view for processing."""
 from django.urls import path
-from .views import upload_view
+from .views import upload_view, home_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('upload/', upload_view, name='upload_view'),
 ]

84dcf716506b0c3b50f88512e78a7ca0b3f43834
dahunsi-dami <gbemigadare@gmail.com>
2024-11-18 19:31:01 +0100

    Create user model leveraging Django's builtin User class.

--- cvtailor_app/models.py
+++ cvtailor_app/models.py
@@ -3,8 +3,22 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class JobApplication(models.Model):
+    """Model for the user's CV and Job description upload."""
     uploaded_file = models.FileField(upload_to='uploads/')
     job_description = models.TextField()
 
     def __str__(self):
         return self.job_description[:50]
+
+
+class User(AbstractUser):
+    """
+    The model for users of the CV Tailor platform.
+
+    It makes Django's built user model for user auth,
+    the User class, to inherit from AbstractUser class,
+    enabling our HTML forms to use Django's built-in user auth.
+    """
+    def __str__(self):
+        """Returns a str representation of the user."""
+        return self.username

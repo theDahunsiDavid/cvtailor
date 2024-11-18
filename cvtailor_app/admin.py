cd2a8d23a51b8b9fcc1cc4e15280a55bcbc27dfc
dahunsi-dami <gbemigadare@gmail.com>
2024-11-18 19:31:37 +0100

    Register the User model in our database in admin.py.

--- cvtailor_app/admin.py
+++ cvtailor_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import JobApplication
+from .models import JobApplication, User
 
 # Register your models here.
 admin.site.register(JobApplication)
+admin.site.register(User)

9b47b89892a2a9aad5af278e8881aa267119a93b
dahunsi-dami <gbemigadare@gmail.com>
2024-11-19 20:18:33 +0100

    Map the URL routes to user auth view in app's urls.py.

--- cvtailor_app/urls.py
+++ cvtailor_app/urls.py
@@ -7,11 +7,14 @@ from .views import implement_suggestion
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('signUp', views.signUp, name='signUp'),
+    path('signIn', views.signIn, name='signIn'),
+    path('signOut', views.signOut, name='signOut'),
     path('upload/', views.upload_cv, name="upload_cv"),
     path('convert_ats/', convert_to_ats_format, name='convert_ats'),
-    path('api/implement_suggestion', implement_suggestion, name='implement_suggestion'),
     # path('download_docx/', views.download_docx, name='download_docx'),
     path('download_pdf/', views.download_pdf, name='download_pdf'),
+    path('api/implement_suggestion', implement_suggestion, name='implement_suggestion'),
 ]
 
 if settings.DEBUG:
